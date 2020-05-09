535ea57219fd7453324ac878b78b961db9b677e0
heyhardie <64929403+heyhardie@users.noreply.github.com>
2020-05-08 18:23:57 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,11 +24,11 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/hardie`,
           },
           {
             name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            url: `https://www.instagram.com/hardie/`,
           },
         ],
       },
@@ -45,7 +45,7 @@ module.exports = {
       options: {
         name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
         short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        description: `Hardie Tankersley's Personal Site`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
